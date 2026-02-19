0dc31710b215312ef7387b90abf73a38a3129aeb
Hugo <104373242+HugoMS655@users.noreply.github.com>
2026-02-19 17:54:01 +0000

    Add different tab creation logic on index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-import "./styles.css";
+import "./styles/styles.css";
 
 const mainContent = document.querySelector("#content");
 
-const headerButtons = document.querySelectorAll(".header-buttons");
+const headerNav = document.querySelectorAll("#nav-menu");
+
+headerNav.addEventListeners("click", (event) => {
+  if (event.target.classList.contains("header-btn")) {
+    switch (event.target.id) {
+      case "home-btn":
+        drawHomeTab();
+        break;
+      case "menu-btn":
+        drawMenuTab();
+        break;
+      case "about-btn":
+        drawAboutUsTab();
+        break;
+      case "contact-btn":
+        drawContactTab();
+        break;
+    }
+  }
+});
+
+function drawHomeTab() {}
+
+function drawMenuTab() {}
+
+function drawAboutUsTab() {}
+
+function drawContactTab() {}
